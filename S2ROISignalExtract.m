2dc83f97aadb0e2d868d5e3cdf607cdd8c368c3d
xiaosonghe <32000944+xiaosonghe@users.noreply.github.com>
2017-09-18 12:51:29 -0400

    Update S2ROISignalExtract.m

--- S2ROISignalExtract.m
+++ S2ROISignalExtract.m
@@ -1,11 +1,10 @@
 % Extract ROI Signal
 % Order: LIFGorb, LIFG, LMFG, LAT, LMAT, LMPT, LPT, LAG, RIFGorb, RIFG, RMFG, RAT, RMAT, RMPT, RPT, RAG
 clear all;
-sbj=importdata('E:\VerbGeneration_network\sbj_all_new.txt');
+sbj=importdata('E:\VerbGeneration_network\sbj.txt');
 parfor i=1:length(sbj)
-	roi_filest = spm_get('files',['E:\VerbGeneration_network\4IndividualROIs\' sbj{i}],'*roi.mat');
+    roi_filest = spm_get('files',['E:\VerbGeneration_network\4IndividualROIs\' sbj{i}],'*roi.mat');
     roi_files=roi_filest([4 3 5 2 6:8 1 12 11 13 10 14:16 9],:);
-%     ImgData=['E:\VerbGeneration_network\2DenoisedData\' sbj{i} '.nii']; 
     ImgData=['E:\VerbGeneration_network\1Preprocessing\FunImgARCWS\' sbj{i} '\swCovRegressed_4DVolume.nii']; 
     pathD=['E:\VerbGeneration_network\5ROISignals\' sbj{i} '.txt'];
     extractROIsignals(roi_files,ImgData,pathD);
